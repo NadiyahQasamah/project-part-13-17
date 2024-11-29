9dc932c47ba9fc31c420a913884068faa30fd8e9
NadiyahQasamah <nadiyahqass@gmail.com>
2024-11-29 11:23:21 +0700

    task

---
+++ database/migrations/2024_11_09_031322_create_articles_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('articles', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('slug')->unique();
+            $table->text(column: 'teaser');
+            $table->text('body');
+            $table->string('meta-title');
+            $table->string('meta-description');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('articles');
+    }
+};
---
+++ resources/views/home.blade.php
@@ -0,0 +1,6 @@
+<x-app-layout>
+    <x-slot name="heading">
+     Home   
+    </x-slot>
+    Home
+</x-app-layout>
